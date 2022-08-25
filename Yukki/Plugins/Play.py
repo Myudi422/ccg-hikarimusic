04fd656850936e67e549daf0466a1654733c0d3f
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-08-25 13:41:25 +0700

    tes

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -34,7 +34,7 @@ loop = asyncio.get_event_loop()
 
 
 @app.on_message(
-    filters.command(["music", f"music@{BOT_USERNAME}"]) & filters.group
+    filters.command(["playm", f"playm@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
